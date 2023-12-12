9fb0c4a924c7c576cc4fb688fa9f5fa242ef4d93
nschutz <nschutz@student.42wolfsburg.de>
2023-12-12 09:57:54 +0100

    feat: checking files

--- parsing/convert_struct.c
+++ parsing/convert_struct.c
@@ -12,10 +12,36 @@
 
 #include "cub2d.h"
 
+
+static int	get_textures(t_map *map, t_input *input)
+{
+	int fd;
+
+	fd = open(input->no_texture, O_RDONLY);
+	if (!fd)
+		return (free_identifier(input), 0);
+	close (fd);
+	fd = open(input->so_texture, O_RDONLY);
+	if (!fd)
+		return (free_identifier(input), 0);
+	close (fd);
+	fd = open(input->we_texture, O_RDONLY);
+	if (!fd)
+		return (free_identifier(input), 0);
+	close (fd);
+	fd = open(input->ea_texture, O_RDONLY);
+	if (!fd)
+		return (free_identifier(input), 0);
+	close (fd);
+	return (1);
+}
+
 int convert_struct(t_input *input)
 {
 	t_map	map;
 
+	if (!get_textures(&map, input))
+		return (0);
 	/*map.no_text;
 	map.so_tex;
 	map.we_tex; get_mlx_pointer;
