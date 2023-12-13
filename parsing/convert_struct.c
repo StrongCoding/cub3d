487c877ea32a974430c285b48638cc25741aa17b
nschutz <nschutz@student.42wolfsburg.de>
2023-12-13 14:43:05 +0100

    feat: changing order of textures

--- parsing/convert_struct.c
+++ parsing/convert_struct.c
@@ -21,41 +21,41 @@ static int	get_textures(t_map *map, t_input *input)
 	ft_printf("calloc double pointer\n");
 	if (!map->textures)
 		return (free_identifier(input), 0);
-	fd = open(input->no_texture, O_RDONLY);
+	fd = open(input->we_texture, O_RDONLY);
 	ft_printf("fd 1\n");
 	if (!fd)
 		return (free_identifier(input), 0);
-	map->textures[0] = ft_newsprite(input->no_texture);
+	map->textures[0] = ft_newsprite(input->we_texture);
 	ft_printf("calloc 1\n");
 	if (!map->textures[0])
 		return (free_identifier(input), 0);
 	close (fd);
-	fd = open(input->so_texture, O_RDONLY);
+	fd = open(input->ea_texture, O_RDONLY);
 	ft_printf("fd 2\n");
 	if (!fd)
 		return (free_identifier(input), 0);
-	map->textures[1] = ft_newsprite(input->so_texture);
+	map->textures[1] = ft_newsprite(input->ea_texture);
 	ft_printf("calloc 2\n");
 	if (!map->textures[1])
-		return (free_identifier(input), 0);
+		return (free_identifier(input), free_sprites(map->textures), 0);
 	close (fd);
-	fd = open(input->we_texture, O_RDONLY);
+	fd = open(input->no_texture, O_RDONLY);
 	ft_printf("fd 3\n");
 	if (!fd)
 		return (free_identifier(input), 0);
-	map->textures[2] = ft_newsprite(input->ea_texture);
+	map->textures[2] = ft_newsprite(input->no_texture);
 	ft_printf("calloc 3\n");
 	if (!map->textures[2])
 		return (free_identifier(input), 0);
 	close (fd);
-	fd = open(input->ea_texture, O_RDONLY);
+	fd = open(input->so_texture, O_RDONLY);
 	ft_printf("fd 4\n");
 	if (!fd)
 		return (free_identifier(input), 0);
-	map->textures[3] = ft_newsprite(input->we_texture);
+	map->textures[3] = ft_newsprite(input->so_texture);
 	ft_printf("calloc 4\n");
 	if (!map->textures[3])
-		return (free_identifier(input), free_sprites(map->textures), 0);
+		return (free_identifier(input), 0);
 	close (fd);
 	return (1);
 }
