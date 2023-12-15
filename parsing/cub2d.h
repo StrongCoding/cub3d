a2f3efc82052550c2008b8a3d0e49afb0a3f7af0
nschutz <nschutz@student.42wolfsburg.de>
2023-12-15 14:00:35 +0100

    feat: norminette cub2d.h

--- parsing/cub2d.h
+++ parsing/cub2d.h
@@ -18,20 +18,19 @@
 # include "../includes/structs.h"
 # include <stdlib.h>
 
-t_input 		read_map(char *name);
-int				ending_cub(char *name);
-int				map_length(char *name);
-void			find_identifier(t_input *input, char **map, int *line);
-void			get_identifier_two(t_input *input, char **map, int *i, int bool);
-int				fl_colors(t_input *input, char *map);
-int				ce_colors(t_input *input, char *map);
-char			**get_map(t_input *input, char **file, int line);
-int				check_map(char **file, int row, int col, int *s_bool);
-int				valid_char(char c);
-int 			convert_struct(t_input *input, t_map *map, t_game *game);
-void			free_array(char **array);
-void			free_identifier(t_input *input);
-void			free_sprites(t_sprite **sprite);
-
+t_input		read_map(char *name);
+int			ending_cub(char *name);
+int			map_length(char *name);
+void		find_identifier(t_input *input, char **map, int *line);
+void		get_identifier_two(t_input *input, char **map, int *i, int bool);
+int			fl_colors(t_input *input, char *map);
+int			ce_colors(t_input *input, char *map);
+char		**get_map(t_input *input, char **file, int line);
+int			check_map(char **file, int row, int col, int *s_bool);
+int			valid_char(char c);
+int			convert_struct(t_input *input, t_map *map, t_game *game);
+void		free_array(char **array);
+void		free_identifier(t_input *input);
+void		free_sprites(t_sprite **sprite);
 
 #endif
